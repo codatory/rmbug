bef42d9918f82744ea78988247420e8dea894847
Mario Danic <mario.danic@gmail.com>
2012-03-11 01:55:35 +0100

    Fixed some more occurances of gitlabhq

--- config/initializers/rmbug/10_load_config.rb
+++ config/initializers/rmbug/10_load_config.rb
@@ -1,3 +1,3 @@
-GIT_HOST = YAML.load_file("#{Rails.root}/config/gitlab.yml")["git_host"]
-EMAIL_OPTS = YAML.load_file("#{Rails.root}/config/gitlab.yml")["email"]
-GIT_OPTS = YAML.load_file("#{Rails.root}/config/gitlab.yml")["git"]
+GIT_HOST = YAML.load_file("#{Rails.root}/config/rmbug.yml")["git_host"]
+EMAIL_OPTS = YAML.load_file("#{Rails.root}/config/rmbug.yml")["email"]
+GIT_OPTS = YAML.load_file("#{Rails.root}/config/rmbug.yml")["git"]
